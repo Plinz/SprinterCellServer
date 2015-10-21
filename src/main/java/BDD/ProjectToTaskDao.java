23afb6f16ed2238893e24c8d3fde6e07ff1e333e
DylanDefives <dyl59.game@gmail.com>
2015-10-21 16:39:46 +0200

    ProjectToTaskDao

---
+++ src/main/java/BDD/ProjectToTaskDao.java
@@ -0,0 +1,24 @@
+package BDD;
+
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+
+public interface ProjectToTaskDao {
+
+	@SqlUpdate("create table projecttasks (idp integer, idt integer)")
+	public void createProjectTask();
+	
+	@SqlUpdate("insert into projecttasks (idp, idt)")
+	@GetGeneratedKeys
+	public int insert(@Bind("idp") int idp, @Bind("idt") int idt);
+	
+	@SqlUpdate("delete from projecttasks where idp = :idp and idt = :idt")
+	public int deleteProjectTask(@Bind("idp") int idp, @Bind("idt") int idt);
+	
+	@SqlUpdate("drop table if exists projecttasks")
+	public void dropProjectTask();
+
+	public void close();
+	
+}
