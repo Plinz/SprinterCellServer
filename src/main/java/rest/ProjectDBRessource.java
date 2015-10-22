b762c88392077c223e6bebfbf86386efdcdd5028
Antoine DUQUENNOY <antoine.duquennoy@etudiant.univ-lille1.fr>
2015-10-22 09:13:13 +0200

    change import

--- src/main/java/rest/ProjectDBRessource.java
+++ src/main/java/rest/ProjectDBRessource.java
@@ -1,20 +1,17 @@
 package rest;
 
-import java.awt.PageAttributes.MediaType;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
