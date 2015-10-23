d4234fc716574ddb3e33674eeaff48e6b87ce5bc
Dowan <thomasgosselin@live.fr>
2015-10-23 14:44:56 +0200

    re

--- src/main/java/servlet/WorkPanel.java
+++ src/main/java/servlet/WorkPanel.java
@@ -1,5 +1,14 @@
 package servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.naming.InitialContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 @WebServlet("servlet/WorkPanel")
